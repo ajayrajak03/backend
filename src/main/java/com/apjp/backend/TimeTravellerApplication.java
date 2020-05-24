471a2cbdbc946a6516e756371a2f2dc95252f1a2
ajayr <ajayr@DESKTOP-PCGNN10>
2020-05-24 18:48:18 +0530

    First commit......

---
+++ src/main/java/com/apjp/backend/TimeTravellerApplication.java
@@ -0,0 +1,14 @@
+package com.apjp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTravellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTravellerApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/apjp/backend/TimeTravellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apjp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTravellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
